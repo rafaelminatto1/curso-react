93c6872910a1be93123fb62608cdb43ee29417bd
rafaelminatto1 <rafael@sateg.com.br>
2019-12-19 22:57:40 -0300

    aula09

--- site/src/actions/contatoActions.js
+++ site/src/actions/contatoActions.js
@@ -1,20 +1,69 @@
-export  const dataOnChange = function (evento){
-    return{
+import axios from 'axios'
+
+const URL = 'http://localhost:3200/api/contatos'
+
+export const dataOnChange = function(evento){
+    return {
         type : 'ATUALIZA_DATA',
         value : evento.target.value
     }
 }
 
-export const nomeOnChange = function (evento){
+export const nomeOnChange = function(evento){
     return{
-        type:'ATUALIZA_NOME',
-        value: evento.target.value
+        type : 'ATUALIZA_NOME',
+        value : evento.target.value
+    }
+}
+
+export const emailOnChange = function(evento){
+    return {
+        type : 'ATUALIZA_EMAIL',
+        value : evento.target.value
+    }
+}
+
+export const telefoneOnChange = function(evento){
+    return {
+        type : 'ATUALIZA_TELEFONE',
+        value : evento.target.value
+    }
+}
+
+export const assuntoOnChange = function(evento){
+    return {
+        type : 'ATUALIZA_ASSUNTO',
+        value : evento.target.value
+    }
+}
+
+export const limpar = function(e){
+    if(e){
+        e.preventDefault()
     }
     
-}
-export const emailOnChange = function (evento){
-    return{
-        type:'ATUALIZA_EMAIL',
-        value: evento.target.value
+    return {
+        type : 'LIMPAR_FORM'
     }
+}
+
+export const adicionar = function(data, nome, email, telefone, assunto){
+    return (dispatch)=>{
+    return axios.post(URL, {
+        data,
+        nome,
+        email,
+        telefone,
+        assunto
+    })
+    .then(_ =>{
+        alert('Contato enviado!')
+        dispatch(limpar())
+        
+    })
+    .catch(error => {
+        console.log(error)
+        alert('Erro ao salvar contato')
+    })
+}
 }
--- site/src/reducers/contatoReducers.js
+++ site/src/reducers/contatoReducers.js
@@ -1,16 +1,19 @@
 const INITIAL_STATE = {
     
-        data: '05-05-1992',
-            nome :'Rafael',
-            email : 'rafael.minatto@yahoo.com.br',
-            telefone: '11975451019',
-            assunto: 'assunto teste'
+        data: '',
+            nome :'',
+            email : '',
+            telefone: '',
+            assunto: ' '
     }
             export default function (state = INITIAL_STATE, action) {
                 switch(action.type){
                     case 'ATUALIZAR_DATA': return {...state,data:action.value}
                     case 'ATUALIZA_NOME':  return {...state,nome : action.value}
                     case 'ATUALIZA_EMAIL':  return {...state,email : action.value}
+                    case 'ATUALIZA_TELEFONE':  return {...state,telefone : action.value}
+                    case 'ATUALIZA_ASSUNTO':  return {...state,assunto : action.value}
+                    case 'LIMPAR_FORM': return INITIAL_STATE
                     default : return state
                 }
                 
